cd10cc1ee7d13a3679a04917d8a021c1281f538f
Zeynep Okyar <zynpokyar@gmail.com>
2021-07-04 21:30:15 +0300

    düzenledim

---
+++ Deneme.cs
@@ -0,0 +1,21 @@
+using System;
+namespace playground
+ {
+    class Deneme
+    {
+        public static void print()
+        {
+           
+        Console.WriteLine("printing");
+            
+
+        }
+         public void write()
+        {
+           
+        Console.WriteLine("write");
+            
+
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace playground
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello Zeynep!");
+           Deneme mydeneme=new Deneme();
+           Deneme.print();
+mydeneme.write();
+        }
+    }
+}
