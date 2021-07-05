dba1e77f9ba67d45a3a78071900158850e4f4d31
Zeynep Okyar <zynpokyar@gmail.com>
2021-07-05 22:20:47 +0300

    düzenlemeler yapıldı

--- Program.cs
+++ Program.cs
@@ -9,20 +9,38 @@ namespace KampIntro
             string kategoriEtiketi ="kategori";
             int ogrenciSayisi = 32000;
             double faizOrani = 1.45;
-            bool sistemeGirisYapmisMi =false;
+            bool sistemeGirisYapmisMi =true;
+            double dolarDun = 7.35;
+            double dolarBugun =7.45;
+
+
+
+            if (dolarDun>dolarBugun)
+            {
+                Console.WriteLine("Azalış butonu");
+            }
+            else if (dolarDun<dolarBugun)
+            {
+             
+                 Console.WriteLine("Artış butonu");
+            }
+            else
+             {
+                 Console.WriteLine("değişmedi butonu");
+             }   
+
+
 
             if (sistemeGirisYapmisMi == true)
             {
                 Console.WriteLine("Kullanıcı ayarları butonu");
             }
-            else
-            {
-                Console.WriteLine("Giriş yap butonu");
-            }
-
-
-
-            Console.WriteLine(kategoriEtiketi);
+            else 
+             {
+                 Console.WriteLine("Giriş  yap butonu");
+             }
+                 
+              Console.WriteLine(kategoriEtiketi);
 
 
           
@@ -30,4 +48,5 @@ namespace KampIntro
 
         }
     }
-}
+ }   
+
